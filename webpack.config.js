3ce4948d9254d73f841cd80ce77bfbf91fdec6d5
Kevin Nielsen <knielsen@alertlogic.com>
2019-08-08 14:51:41 -0700

    Added some ommitted .js files and changed the internal name of the module from @al/haversack to @al/common.

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const nodeExternals = require("webpack-node-externals" );
+
+module.exports = {
+  mode: 'production',
+  target: 'node',
+  entry: {
+    'index': path.resolve( __dirname, './dist/commonjs/index.js' ),
+    "locator": path.resolve( __dirname, "./dist/commonjs/locator/index.js" ),
+    "schema-validator": path.resolve( __dirname, "./dist/commonjs/schema-validator/index.js" )
+  },
+  externals: [ nodeExternals() ],
+  output: {
+    path: path.resolve(__dirname, './'),
+    filename: '[name].js',
+    library: '@al/common/[name]',
+    libraryTarget: 'umd', // supports commonjs, amd and web browsers
+    globalObject: 'this'
+  },
+  plugins: []
+};
