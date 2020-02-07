f115d24d483414e9c424a395d72fb5449d41153d
scottarver <scottarver@gmail.com>
2020-02-07 12:44:23 -0600

    Update al-location.dictionary.ts

--- src/locator/al-location.dictionary.ts
+++ src/locator/al-location.dictionary.ts
@@ -243,7 +243,7 @@ export const AlLocationDictionary: AlLocationDescriptor[] =
     },
     {
         locTypeId: AlLocation.IrisUI,
-        uri: 'https://iris-ui--production-staging-uk.ui-dev.product.dev.alertlogic.com',
+        uri: 'https://iris-ui-production-staging-uk.ui-dev.product.dev.alertlogic.com',
         environment: 'production-staging',
         residency: 'EMEA',
         keyword: 'iris',
